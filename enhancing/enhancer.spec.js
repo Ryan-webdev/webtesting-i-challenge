37a9c4a291e125cd7ade30add273f713713781c9
Ryan Schueck <ryschueck@gmail.com>
2020-09-08 18:35:54 -0400

    Create: Created a test for the repair method

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,3 +1,22 @@
 const enhancer = require('./enhancer.js');
 // test away!
-test("")
+
+const Item = {
+    name: 'Iron Sword',
+    durability: 60,
+    enhancement: 10,
+}
+
+//Repair Item test
+
+describe('Enhancer', () =>{
+    describe('repair() method', () =>{
+        it('should take in an object to repair, and output an object with a durability of 100', function(){
+            let Weapon = enhancer.repair(Item);
+            
+            //expect(Weapon.durability).toBe(2) -- expect to fail
+            expect(Weapon.durability).toBe(100)
+        })
+    })
+})
+
